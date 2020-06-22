1734b296f425e8087f90e8e0be1a76ba8bc34c05
Cole Cassise <cole.cassise@outlook.com>
2020-06-21 20:42:56 -0500

    Improve CPU performance
    Convert static methods to instance variables in CPU

--- src/standard-controller.js
+++ src/standard-controller.js
@@ -7,53 +7,51 @@
 
 class StandardController {
     constructor() {
-        this.data = 0x00;  // Current buttons that are pressed.
-        this.register = 0x00;  // Data that will be sent.
+        this._data = 0x00;  // Current buttons that are pressed.
+        this._register = 0x00;  // Data that will be sent.
         // When true the register will continuously be updated with controller data.
         this._isUpdateRegister = false;
-    }
 
-    get data() { return this._data; }
-    set data(v) { this._data = uint8(v); }
-
-    // 8 bit shift register.
-    get register() { return this._register; }
-    set register(v) { this._register = uint8(v); }
-
-    static get KEYS() {
-        return {
-            A:      1 << 0,
-            B:      1 << 1,
+        this.KEYS = {
+            A: 1 << 0,
+            B: 1 << 1,
             Select: 1 << 2,
-            Start:  1 << 3,
-            Up:     1 << 4,
-            Down:   1 << 5,
-            Left:   1 << 6,
-            Right:  1 << 7,
+            Start: 1 << 3,
+            Up: 1 << 4,
+            Down: 1 << 5,
+            Left: 1 << 6,
+            Right: 1 << 7,
         };
     }
 
+    get _data() { return this.__data; }
+    set _data(v) { this.__data = uint8(v); }
+
+    // 8 bit shift register.
+    get _register() { return this.__register; }
+    set _register(v) { this.__register = uint8(v); }
+
     keydown(key) {
-        this.data |= StandardController.KEYS[key];
+        this._data |= this.KEYS[key];
         this._updateRegister();
     }
 
     keyup(key) {
-        this.data &= ~StandardController.KEYS[key];
+        this._data &= ~this.KEYS[key];
         this._updateRegister();
     }
 
     onRead() {
-        const result = this.register & 0x01;
-        this.register >>= 1;
-        this.register |= (1 << 7); // Subsequent reads returns 1.
+        const result = this._register & 0x01;
+        this._register >>= 1;
+        this._register |= (1 << 7); // Subsequent reads returns 1.
         return result;
     }
 
     onWrite(data) {
         if (data & 0x01) {
             this._isUpdateRegister = true;
-            this.register = this.data;
+            this._register = this._data;
         } else {
             this._isUpdateRegister = false;
         }
@@ -61,7 +59,7 @@ class StandardController {
 
     _updateRegister() {
         if (this._isUpdateRegister) {
-            this.register = this.data;
+            this._register = this._data;
         }
     }
 }
